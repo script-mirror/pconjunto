64f9f3f388aabc1e7c5e575b35a59c18af6ef1c9
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-16 11:20:29 -0300

    feat: buscando e configurando arquivos de entrada do pconjunto dos utlimos 200 dias via webhook sintegre

---
+++ setup_modelos.py
@@ -0,0 +1,42 @@
+from middle.s3 import get_latest_webhook_product, handle_webhook_file
+import datetime
+import os
+import zipfile
+import concurrent.futures
+from middle.utils import Constants
+constants = Constants()
+os.makedirs(constants.PATH_TMP, exist_ok=True)
+
+
+def processar_item(item, nome_modelo):
+    output = handle_webhook_file(item, constants.PATH_TMP)
+    if '.zip' in output:
+        with zipfile.ZipFile(os.path.join(constants.PATH_TMP, output), 'r') as zf:
+            arquivo_para_extrair = [i for i in zf.namelist() if "_m_" in i.lower()][0]
+            zf.extract(arquivo_para_extrair, f"./{nome_modelo}")
+        os.remove(output)
+    else:
+        os.popen(f"mv {output} {nome_modelo}/")
+    return f"Processado: {item['dataProduto']}"
+
+
+def processar_webhook(nome_webhook: str, nome_modelo: str, date_range=200, max_workers=os.cpu_count()*10):
+    items = get_latest_webhook_product(nome_webhook, datetime.datetime.now(), date_range=date_range)
+    
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(processar_item, item, nome_modelo) for item in items]
+        
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+                print(result)
+            except Exception as e:
+                print(f"Erro no processamento: {e}")
+
+
+if __name__ == "__main__":
+    processar_webhook("Modelo ETA", "./Arq_Entrada/ETA40")
+    processar_webhook("Modelo ECMWF", "./Arq_Entrada/ECMWF")
+    processar_webhook("Modelo GEFS", "./Arq_Entrada/GEFS")
+    processar_webhook("Precipitação por Satélite – ONS", "./Arq_Entrada/Observado")
+    processar_webhook("Precipitação por Satélite.", "./Arq_Entrada/Observado")
