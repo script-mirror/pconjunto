8bb67d38841a1f4f0342bd4d09a9e9e9c428239c
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-23 13:15:51 -0300

    feat: parando execucao em caso de exception

--- main.sh
+++ main.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+set -e
+
 data_rodada="$1"
 
+# Executa o primeiro comando Python e verifica se houve erro
 python3 main.py process_input "${data_rodada}"
+if [ $? -ne 0 ]; then
+    echo "Erro ao executar process_input. Encerrando execução."
+    exit 1
+fi
 
+# Executa o script R
 Rscript ./Codigos_R/Roda_Conjunto_V3.4.R
+if [ $? -ne 0 ]; then
+    echo "Erro ao executar script R. Encerrando execução."
+    exit 1
+fi
 
+# Executa o segundo comando Python
 python3 main.py process_output "${data_rodada}"
+if [ $? -ne 0 ]; then
+    echo "Erro ao executar process_output. Encerrando execução."
+    exit 1
+fi
