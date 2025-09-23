5f51cd8df439fca43a1360e26a715e8796235ecd
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-23 13:52:58 -0300

    feat: executando setup modelos junto do flux

--- main.sh
+++ main.sh
@@ -4,21 +4,25 @@ set -e
 
 data_rodada="$1"
 
-# Executa o primeiro comando Python e verifica se houve erro
+
+python3 setup_modelos.py
+if [ $? -ne 0 ]; then
+    echo "Erro ao executar setup_modelos. Encerrando execução."
+    exit 1
+fi
+
 python3 main.py process_input "${data_rodada}"
 if [ $? -ne 0 ]; then
     echo "Erro ao executar process_input. Encerrando execução."
     exit 1
 fi
 
-# Executa o script R
 Rscript ./Codigos_R/Roda_Conjunto_V3.4.R
 if [ $? -ne 0 ]; then
     echo "Erro ao executar script R. Encerrando execução."
     exit 1
 fi
 
-# Executa o segundo comando Python
 python3 main.py process_output "${data_rodada}"
 if [ $? -ne 0 ]; then
     echo "Erro ao executar process_output. Encerrando execução."
